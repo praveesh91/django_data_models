bc721e5d46a76befc7cd3f8ffc4d46ed0efbb08d
praveesh91 <praveesh91@gmail.com>
2025-06-30 01:11:12 +0530

    replace url with reverse function

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
 # Create your models here.
 class Book(models.Model):
@@ -10,3 +11,6 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
+    
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.id])
