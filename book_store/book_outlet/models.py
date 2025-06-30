633c8f876f7a723afa3cec2d4d8909b2d09f73af
praveesh91 <praveesh91@gmail.com>
2025-06-30 21:05:37 +0530

    slug can be blank when entered from admin panel

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -6,10 +6,10 @@ from django.utils.text import slugify
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    author = models.CharField(null=True,max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=100)
     is_best_selling = models.BooleanField(default=False)
-    slug = models.SlugField(default="",null=False,db_index=True)
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
